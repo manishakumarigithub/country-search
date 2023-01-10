d16c669f2d7e3a964769d436304847fe5ac6b0b0
Manisha <monu19nov@gmail.com>
2023-01-10 12:31:55 +0100

    again deploy

--- src/components/Search/SearchForm.tsx
+++ src/components/Search/SearchForm.tsx
@@ -2,8 +2,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
+
 //mui
 import TextField from "@mui/material/TextField";
 //component
